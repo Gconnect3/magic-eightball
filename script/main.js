8373ff38bdc56c11ff070528177dec7583db5f26
GeorgeT <90588054+Gconnect3@users.noreply.github.com>
2022-02-03 18:56:07 -0500

    added magic eight ball phrases, made sure the function worked

--- script/main.js
+++ script/main.js
@@ -1,40 +1,51 @@
 document.querySelector('#button').addEventListener("click", ball)
 
 function ball(){
-    let randomNumber = Math.floor(Math.random() * 15)
+    let randomNumber = Math.floor(Math.random() * 20)
     let ballResult = ""
     
     switch (randomNumber){
-        case 0 : ballResult = "zero"
+        case 0 : ballResult = " It is certain."
         break;
-        case 1 : ballResult = "one"
+        case 1 : ballResult = "As I see it, yes."
         break;
-        case 2 : ballResult = "two"
+        case 2 : ballResult = "Reply hazy, try again."
         break;
-        case 3 : ballResult = "three"
+        case 3 : ballResult = "Don't count on it."
         break;
-        case 4 : ballResult = "four"
+        case 4 : ballResult = "It is decidedly so."
         break;
-        case 5 : ballResult = "five"
+        case 5 : ballResult = "Most likely."
         break;
-        case 6 : ballResult = "six"
+        case 6 : ballResult = "Ask again later."
         break;
-        case 7 : ballResult = "seven"
+        case 7 : ballResult = "My reply is no."
         break;
-        case 8 : ballResult = "eight"
+        case 8 : ballResult = "Without a doubt."
         break;
-        case 9 : ballResult = "nine"
+        case 9 : ballResult = "Outlook good."
         break;
-        case 10 : ballResult = "ten"
+        case 10 : ballResult = "Better not tell you now."
         break;
-        case 11 : ballResult = "eleven"
+        case 11 : ballResult = "My sources say no."
         break;
-        case 12 : ballResult = "twelve"
+        case 12 : ballResult = "Yes definitely."
         break;
-        case 13 : ballResult = "thirteen"
+        case 13 : ballResult = "Yes."
         break;
-        case 14 : ballResult = "fourteen"
+        case 14 : ballResult = "Cannot predict now."
         break;
+        case 15 : ballResult = "Outlook not so good."
+        break;
+        case 16 : ballResult = "You may rely on it."
+        break;
+        case 17 : ballResult = "Signs point to yes."
+        break;
+        case 18 : ballResult = "Concentrate and ask again."
+        break;
+        case 19 : ballResult = "Very doubtful."
+        break;
+        
         default: ballResult = "impossible"
         break;
     }
